7fb4d4a749708b3098817adcdb800e4c0782a617
Kam Khan Sang <riskkam01@gmail.com>
2023-09-10 11:40:20 +0630

    vault backup: 2023-09-10 11:40:19

---
+++ Password Cracking/md5_encode.py
@@ -0,0 +1,14 @@
+import hashlib
+
+# initializing string
+str2hash = "admin"
+
+# encoding admin using encode()
+# then sending to md5()
+result = hashlib.md5(str2hash.encode())
+
+# printing the equivalent hexadecimal value.
+print("The hexadecimal equivalent of hash is : ", end ="")
+print(result.hexdigest())
+
+# https://www.geeksforgeeks.org/md5-hash-python/
